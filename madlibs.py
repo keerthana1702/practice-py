21b03fe3fda23d780dd81a83ae0931b2482080f5
keerthi <keerthanass2002@gmail.com>
2023-06-04 21:55:37 +0530

    new projects are being added

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# #MADLIBS
+# #fill in the blanks or find the missing letters
+# #logic using string concatination
+# youtuber="keerthi"
+# print("subscribe to the" +youtuber)
+# print("subscribe to the{}".format(youtuber))
+# print(f"subscribe to the {youtuber}") FSTRING
+# / is used to indicate continuation or next line
+ex = input("how u feel: ")
+con = input("where u are: ")
+wht = input("what: ")
+madlib = f"reading book is so {ex}!, it gets me involved {con}. this helps \
+me to {wht}"
+print(madlib)
